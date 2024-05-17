fb2a7fc426531ad28ed05aaa24de46478a901a40
Md Anwar Alam <mdanwar40212@gmail.com>
2024-05-17 09:07:14 +0530

    First commmit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+ import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <nav>
+          <div className='logo'>
+          <img src="/images/brand_logo.png" alt="logo" />
+          </div>
+          <ul>
+            <li>Menu</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          <button>Login</button>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
